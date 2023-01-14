73db7c2cc4a12f09f9c8b021802bea14502e5164
Tali Tesar <tali.tesar@gmail.com>
2023-01-14 11:42:09 -0600

    fixing table creation document

--- EmployeeSQL/table_creation.sql
+++ EmployeeSQL/table_creation.sql
@@ -1,3 +1,11 @@
+CREATE TABLE Titles
+(title_id varchar(10) PRIMARY KEY,
+title varchar(30)); 
+
+CREATE TABLE Departments 
+(dept_no varchar(10) PRIMARY KEY NOT NULL,
+dept_name varchar(30) NOT NULL);
+
 CREATE TABLE Employees
 (emp_no int PRIMARY KEY NOT NULL,
 emp_title_id varchar(10),
@@ -5,26 +13,24 @@ birth_date varchar(10),
 first_name varchar(30),
 last_name varchar(30),
 sex varchar(10),
-hire_date varchar(10));
-
-CREATE TABLE Titles
-(title_id varchar(10) PRIMARY KEY, 
-title varchar(30));
+hire_date varchar(10),
+FOREIGN KEY emp_title_id REFERENCES Titles.title_id);
 
 CREATE TABLE dept_emp 
 (emp_no int NOT NULL,
 dept_no varchar(10) NOT NULL,
-PRIMARY KEY(emp_no, dept_no));
-
-CREATE TABLE Departments 
-(dept_no varchar(10) PRIMARY KEY NOT NULL,
-dept_name varchar(30) NOT NULL);
+PRIMARY KEY(emp_no, dept_no),
+FOREIGN KEY emp_no REFERENCES Employees.emp_no,
+FOREIGN KEY dept_no REFERENCES Departments.dept_no);
 
 CREATE TABLE Salaries 
 (emp_no int PRIMARY KEY,
-salary int);
+salary int,
+FOREIGN KEY emp_no REFERENCES Employees.emp_no);
 
 CREATE TABLE dept_manager
 (dept_no varchar(10) NOT NULL,
 emp_no int NOT NULL,
-PRIMARY KEY(dept_no, emp_no));
+PRIMARY KEY(dept_no, emp_no),
+FOREIGN KEY emp_no REFERENCES Employees.emp_no,
+FOREIGN KEY dept_no REFERENCES Departments.dept_no);
